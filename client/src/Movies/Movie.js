9db79f4b15663f1d9e9b0fafaac37af0a14b78f5
Ahmad Guenoun <amguenoun@gmail.com>
2019-08-20 15:36:08 -0400

    added save movie functionality

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -21,23 +21,21 @@ const Movie = (props) => {
   }, [props.match.params.id]);
 
   // Uncomment this only when you have moved on to the stretch goals
-  // const saveMovie = () => {
-  //   const addToSavedList = props.addToSavedList;
-  //   addToSavedList(movie)
-  // }
+  const saveMovie = () => {
+    const addToSavedList = props.addToSavedList;
+    addToSavedList(movie)
+  }
 
   if (!movie) {
     return <div>Loading movie information...</div>;
   }
   const { title, director, metascore, stars } = movie;
 
-  if (!stars) {
-    return <div>Loading...</div>;
-  }
-  return (
+
+  return (!stars ? <div>Loading...</div> :
     <div className="save-wrapper">
       <MovieCard key={movie.id} movie={movie} />
-      <div className="save-button">Save</div>
+      <div className="save-button" onClick={() => saveMovie()}>Save</div>
     </div>
   );
 }
